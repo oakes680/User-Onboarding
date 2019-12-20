b64a86c837bfdc3d4016238011985f671770e3d2
oakes680 <chris.oakes@outlook.com>
2019-12-19 16:22:11 -0800

    done mvp

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import FormikForm from './components/Form'
+
+
+function App() {
+  return (
+   <FormikForm />
+  );
+};
+
+
+export default App;
+
+
+// name email password ,  Terms of service checkbox.  submit button to send our form data to the server
